e18559eee2c7cca96bdffcb204d223f0b31f4af3
BW <be.waszkiewicz@gmail.com>
2019-01-22 21:17:13 +0100

    still session tests

--- script/sessionMechanism.js
+++ script/sessionMechanism.js
@@ -1,6 +1,14 @@
 function checkSession() {
     if (sessionStorage.getItem("SignIn") == "1"){
-        document.write("Hello World!");
+        document.getElementById("LinkBarTableFirstLink").href="userPanel.html";
+        document.getElementById("LinkBarTableFirstText").innerHTML="User Panel";
+        document.getElementById("LinkBarTableSecondLink").href="stories.html";
+        document.getElementById("LinkBarTableFirstText").innerHTML="Logout";
+    } else {
+        document.getElementById("LinkBarTableFirstLink").href="SignIn.html";
+        document.getElementById("LinkBarTableFirstText").innerHTML="Sign In ";
+        document.getElementById("LinkBarTableSecondLink").href="SignUp.html";
+        document.getElementById("LinkBarTableFirstText").innerHTML=" Sign Up";
     }
 
 }
