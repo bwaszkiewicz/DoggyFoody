ce937ef09d8bbd77de80481e90a3a18beade5152
BW <be.waszkiewicz@gmail.com>
2019-01-22 21:18:19 +0100

    fix

--- script/sessionMechanism.js
+++ script/sessionMechanism.js
@@ -3,12 +3,12 @@ function checkSession() {
         document.getElementById("LinkBarTableFirstLink").href="userPanel.html";
         document.getElementById("LinkBarTableFirstText").innerHTML="User Panel";
         document.getElementById("LinkBarTableSecondLink").href="stories.html";
-        document.getElementById("LinkBarTableFirstText").innerHTML="Logout";
+        document.getElementById("LinkBarTableSecondText").innerHTML="Logout";
     } else {
         document.getElementById("LinkBarTableFirstLink").href="SignIn.html";
         document.getElementById("LinkBarTableFirstText").innerHTML="Sign In ";
         document.getElementById("LinkBarTableSecondLink").href="SignUp.html";
-        document.getElementById("LinkBarTableFirstText").innerHTML=" Sign Up";
+        document.getElementById("LinkBarTableSecondText").innerHTML=" Sign Up";
     }
 
 }
