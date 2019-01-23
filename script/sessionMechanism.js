c99212a937e04129d4b5c46c191e3228a63a7966
BW <be.waszkiewicz@gmail.com>
2019-01-23 18:05:01 +0100

    +SignUp session mechanism

--- script/sessionMechanism.js
+++ script/sessionMechanism.js
@@ -49,5 +49,18 @@ function SignInCheckSession() {
 }
 
 function SignUpCheckSession() {
+    if(sessionStorage.getItem("SignIn") == "1"){
+        document.getElementById("LinkBarTableFirstLink").href="UserPanel.html";
+        document.getElementById("LinkBarTableFirstText").innerHTML="User Panel";
+        document.getElementById("LinkBarTableSecondLink").href="Logout.html";
+        document.getElementById("LinkBarTableSecondText").innerHTML="Logout";
 
+        document.getElementById("signUpMainBlockDiv").innerHTML="You are already sign in!";
+    } else {
+        document.getElementById("LinkBarTableFirstLink").href="SignIn.html";
+        document.getElementById("LinkBarTableFirstText").innerHTML="Sign In ";
+        document.getElementById("LinkBarTableSecondLink").href="SignUp.html";
+        document.getElementById("LinkBarTableSecondText").innerHTML=" Sign Up";
+    
+    }
 }
